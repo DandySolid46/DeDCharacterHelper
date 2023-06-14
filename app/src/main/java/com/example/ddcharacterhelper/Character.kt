fda225f5e2b031406fd5cf62d6d0375878c4729c
DandySolid46 <lucasliduino@gmail.com>
2023-06-13 21:37:41 -0300

    Project Start

---
+++ app/src/main/java/com/example/ddcharacterhelper/Character.kt
@@ -0,0 +1,15 @@
+package com.example.ddcharacterhelper
+
+class Character {
+    var name = ""
+    var dEdClass = ""
+    var race = ""
+
+    var intelligence = 8
+    var constitution = 8
+    var strength = 8
+    var charisma = 8
+    var wisdom = 8
+    var dexterity = 8
+
+}
---
+++ app/src/main/java/com/example/ddcharacterhelper/Choose_class_activity.kt
@@ -0,0 +1,11 @@
+package com.example.ddcharacterhelper
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Choose_class_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.choose_class_activity)
+    }
+}
